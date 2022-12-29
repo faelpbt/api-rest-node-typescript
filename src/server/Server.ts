ba41c25431cb3bad6ca72eb5f3b0969ac377b6ee
Rafael Botelho <rafapereirabt@gmail.com>
2022-12-29 15:04:04 -0300

    feat: added code style configs

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const server = express();
 
+interface Teste {
+  
+}
+
 server.get('/', (req, res) => {
-  return res.send('Olá, DEV!');
+	return res.send('Olá, DEV!');
 });
 
 export { server };
